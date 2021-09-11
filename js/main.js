9d207fed1529b75a1ae27c2dd873db84eaefdfa7
chymakoleg <chymakoleg@gmail.com>
2021-09-12 01:03:02 +0300

    getting started with js

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+let a = 100;
+let d = 10;
+let c = 5;
+
+c *=2;
+a -=100;
+let x = c / 10;
+
+let result = a + x * c - d;
+console.log ('result', result);
+
+
+
+
+ let h = 40;
+ let w = 40;
+ h = h * 5;
+ w = w * 8;
+
+let r = h + w;
+console.log ('result2', r);
+
+
